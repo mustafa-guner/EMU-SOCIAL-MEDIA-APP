0b371ae580b4a335f4749624a4a2b481c9d4806f
Slex1one-Musdy <legend123412@outlook.com>
2023-01-24 10:52:51 +0200

    create laravel api function to fetch users by id and update

--- EMUSocialAPP/routes/api.php
+++ EMUSocialAPP/routes/api.php
@@ -13,7 +13,83 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get("/admin/accounts/{id}",function (Request $request){
+    $accessToken = $_COOKIE["accessToken"];
+    $sessionToken = $_COOKIE["sessionToken"];
+    $curlHandler = curl_init();
+    curl_setopt_array($curlHandler, [
+    CURLOPT_URL => 'http://localhost:4200/api/v1/admin/users/'.$request->id,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_COOKIE => "accessToken={$accessToken};sessionToken={$sessionToken}",
+     ]);
+    $response = curl_exec($curlHandler);
+    curl_close($curlHandler);
+    $result = json_decode($response);
+    return $result;
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get("/admin/students/{userId}",function (Request $request){
+    $accessToken = $_COOKIE["accessToken"];
+    $sessionToken = $_COOKIE["sessionToken"];
+    $curlHandler = curl_init();
+    curl_setopt_array($curlHandler, [
+    CURLOPT_URL => 'http://localhost:4200/api/v1/admin/students/'.$request->userId,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_COOKIE => "accessToken={$accessToken};sessionToken={$sessionToken}",
+     ]);
+    $response = curl_exec($curlHandler);
+    curl_close($curlHandler);
+    $result = json_decode($response);
+    return $result;
+});
+
+Route::get("/admin/staffs/{staffId}",function (Request $request){
+    $accessToken = $_COOKIE["accessToken"];
+    $sessionToken = $_COOKIE["sessionToken"];
+    $curlHandler = curl_init();
+    curl_setopt_array($curlHandler, [
+    CURLOPT_URL => 'http://localhost:4200/api/v1/admin/staffs/'.$request->staffId,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_COOKIE => "accessToken={$accessToken};sessionToken={$sessionToken}",
+     ]);
+    $response = curl_exec($curlHandler);
+    curl_close($curlHandler);
+    $result = json_decode($response);
+    return $result;
+});
+
+Route::post("/admin/update-user/{userId}",function (Request $request){
+    $accessToken = $_COOKIE["accessToken"];
+    $sessionToken = $_COOKIE["sessionToken"];
+
+    $curlHandler = curl_init();
+    curl_setopt($curlHandler, CURLOPT_POST, true);
+    curl_setopt_array($curlHandler, [
+    CURLOPT_URL => 'http://localhost:4200/api/v1/admin/update-user/'.$request->userId,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_COOKIE => "accessToken={$accessToken};sessionToken={$sessionToken}",
+     ]);
+     curl_setopt($curlHandler, CURLOPT_POSTFIELDS, $request->all());
+    $response = curl_exec($curlHandler);
+    curl_close($curlHandler);
+    $result = json_decode($response);
+    return $result;
+});
+
+Route::post("/admin/toggle-activation-user/{userId}",function (Request $request){
+    $accessToken = $_COOKIE["accessToken"];
+    $sessionToken = $_COOKIE["sessionToken"];
+
+    $curlHandler = curl_init();
+    curl_setopt($curlHandler, CURLOPT_POST, true);
+    curl_setopt_array($curlHandler, [
+    CURLOPT_URL => 'http://localhost:4200/api/v1/admin/toggle-activation-user/'.$request->userId,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_COOKIE => "accessToken={$accessToken};sessionToken={$sessionToken}",
+     ]);
+     curl_setopt($curlHandler, CURLOPT_POSTFIELDS, $request->all());
+    $response = curl_exec($curlHandler);
+    curl_close($curlHandler);
+    $result = json_decode($response);
+    return $result;
 });
