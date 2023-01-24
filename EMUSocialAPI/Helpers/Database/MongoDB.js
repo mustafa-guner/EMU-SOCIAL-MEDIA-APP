94b7a9d26ff5742cc705db9e43fe2dbc67e1c7d1
Slex1one-Musdy <legend123412@outlook.com>
2023-01-24 10:53:44 +0200

    create a collection on cloud for mongo db and add it to env file to check if current envirionment is development, use local one, else use cloud one.

--- EMUSocialAPI/Helpers/Database/MongoDB.js
+++ EMUSocialAPI/Helpers/Database/MongoDB.js
@@ -2,9 +2,18 @@ const mongoose = require("mongoose");
 
 const connectToMongoDB = async() => {
     try {
-        const { MONGO_DB_CONNECTION_URL } = process.env;
+        const {
+            MONGO_DB_CONNECTION_URL,
+            MONGO_DB_CONNECTION_PRODUCTION_URL,
+            NODE_ENV,
+        } = process.env;
+        const MONGO_DB_CONNECTION =
+            NODE_ENV === "production" ?
+            MONGO_DB_CONNECTION_PRODUCTION_URL :
+            MONGO_DB_CONNECTION_URL;
+
         mongoose.set("strictQuery", false);
-        await mongoose.connect(MONGO_DB_CONNECTION_URL, {
+        await mongoose.connect(MONGO_DB_CONNECTION, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
